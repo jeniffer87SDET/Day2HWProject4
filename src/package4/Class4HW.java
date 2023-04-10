e653cf0b692de78092acbba9e6f3d4ee2522ade3
Rimsky <Rimsky@DESKTOP-BO2IQFC.lan>
2023-04-09 21:47:25 -0400

    "Let's practice a little more!"

---
+++ src/package4/Class4HW.java
@@ -0,0 +1,13 @@
+package package4;
+
+public class Class4HW {
+	
+	public static void main (String args []) {
+		System.out.println("Homework - part 4");
+	}
+	
+	public void add2(int a, int b) {
+		System.out.println(a + b);
+	}
+
+}
